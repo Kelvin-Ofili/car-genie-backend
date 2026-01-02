78113a00fd7f3df112cae762a4c800cb344e89c5
Kelvin <kelvinofili1@gmail.com>
2026-01-02 22:32:10 +0100

    Redeploy attempt

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,12 +1,18 @@
 import admin from "firebase-admin";
+import * as fs from "fs";
+import * as path from "path";
 
 // Load service account from environment variable in production, file in development
 const getServiceAccount = () => {
 	if (process.env.FIREBASE_SERVICE_ACCOUNT) {
 		return JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 	}
-	// Development: load from file
-	return require("../serviceAccountKey.json");
+	// Development: load from file if it exists
+	const serviceAccountPath = path.join(__dirname, "../serviceAccountKey.json");
+	if (fs.existsSync(serviceAccountPath)) {
+		return JSON.parse(fs.readFileSync(serviceAccountPath, "utf-8"));
+	}
+	throw new Error("Firebase service account not found. Set FIREBASE_SERVICE_ACCOUNT environment variable.");
 };
 
 admin.initializeApp({
